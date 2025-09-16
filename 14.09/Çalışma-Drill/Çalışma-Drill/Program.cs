9a0e12c3458647907d70f579007e678e24728948
recepyucegit <recep.yuce1789@gmail.com>
2025-09-16 08:59:43 +0300

    .
    .

---
+++ 14.09/Çalışma-Drill/Çalışma-Drill/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Rectangle
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    // Implement a constructor that takes two parameters: width and height and sets the Width and Height properties.
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    // Implement a method called CalculateArea that returns the area of the rectangle (Width * Height).
+
+    public double CalculateArea()
+    {
+        return Width * Height;
+    }
+}
